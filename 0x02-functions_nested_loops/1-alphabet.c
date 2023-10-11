47cf7e895a9d2c9304ba0e59f4f2170c295a6d70
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-11 02:00:58 +0100

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function to make the alphabet
+ * return(void)
+ */
+	void print_alphabet(void)
+{
+	char r;
+	for (r = 'a'; r <= 'z'; r++) {
+		_putchar(r);
+	}
+		_putchar('\n');
+
+}
