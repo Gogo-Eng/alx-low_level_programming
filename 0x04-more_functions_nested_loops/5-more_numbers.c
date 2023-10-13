68eba82a63a7d106b8bf1f8f76dacc5b26f95d86
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-13 15:33:11 +0100

    it's the finger of God in my life

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
- * more_numbers - a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * more_numbers - prints from 0 to 14, 10 times , followed by a new line.
  * Description - the function takes no argument
- *  * return - doesn't return any value
+ * return - doesn't return any value
+ */
+
+void more_numbers(void)
+{
+	char d;
+	char e;
+	int c = 0;
+
+	while (c < 10)
+	{
+
+		for (d = 0; d <= 14; d++)
+		{
+			e = d;
+			if (d > 9)
+			{
+				_putchar ('1');
+				e = d % 10;
+			}
+			_putchar ('0' + e);
+		}
+		_putchar ('\n');
+		c++;
+	}
+}
