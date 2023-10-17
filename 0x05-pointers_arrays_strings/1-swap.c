fa2c54a473bd4f24156dacbf1f1737f967a07d71
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-17 02:50:15 +0100

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int -  a function that swaps the values of two integers.
+ * @a: the first parameter passed to the function
+ * @b: the second parameter passed to the function
+ * Return: returns nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,5 @@
  #define My_Header
 
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
 #endif
