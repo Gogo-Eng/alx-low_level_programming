cd3c0c6737c7466d398c99b9e2792a7220a0cdf8
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-18 16:58:45 +0100

    a little bit confusing

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,15 +15,11 @@ void print_array(int *a, int n)
 	for (t = 0; t < n; t++)
 	{
 		printf("%d", a[t]);
-
-		if (t < (n - 1))
+		if (t < (n-1))
 		{
 			printf(", ");
 		}
 	}
-
-
-
-
-
+	printf("\n");
 }
+
