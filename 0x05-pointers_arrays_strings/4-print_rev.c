039c11a13559bdc765c98805f45f8d8026764699
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-18 01:52:54 +0100

    the reverse printer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: the pointer parameter passed to the function
+ * Return: returns nothings
+ */
+
+void print_rev(char *s)
+{
+	int size;
+	int i;
+
+	size = 0;
+	while (s[size])
+	{
+		size++;
+	}
+
+	i = size - 1;
+	while (i >= 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
