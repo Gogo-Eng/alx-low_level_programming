ed4b9599cfe8e20663f4a299f9d9face2733c078
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-20 09:31:02 +0100

    i love what i do

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,24 +5,27 @@
  * @dest: first parameter of the function
  * @src: second parameter of the function
  * @n: third parameter of the function
+ * Return: returns a string of characters
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int q;
-	int w;
-
-	w = 0;
-	while (src[w])
-	{
-		w++;
-	}
 
 	q = 0;
-	while (q < n)
+	while (q < n && src[q])
 	{
 		dest[q] = src[q];
 		q++;
 	}
+
+	while (q < n)
+	{
+		dest[q] = '\0';
+		q++;
+	}
+
+	dest[q] = '\0';
 	return (dest);
 }
+
