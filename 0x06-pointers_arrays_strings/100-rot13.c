4a5e8bda090b53e073265c32294c1133493af4bc
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-23 09:07:25 +0100

    arrays and pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 -  a function that encodes a string using rot13.
+ *
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,5 +2,50 @@
 #include <stdio.h>
 
 /**
- * 
- *
+ * cap_string -  a function that capitalizes all words of a string.
+ * @upper: the parameter of the function
+ * Return: returns a string of character
+ */
+
+char *cap_string(char *upper)
+{
+	int y;
+	int capital = 1;
+
+	y = 0;
+	while (upper[y])
+	{
+		char current = upper[y];
+
+		if ((current >= 'a' && current <= 'z') || (current >= 'A' && current <= 'Z'))
+		{
+			if (capital)
+			{
+				if (current >= 'a' && current <= 'z')
+				{
+					upper[y] = current - ('a' - 'A');
+				}
+
+				capital = 0;
+			}
+			else if (current >= 'A' && current <= 'Z')
+			{
+				upper[y] = current - ('A' - 'a');
+
+			}
+
+		}
+
+
+		else
+		{
+			capital = 1;
+		}
+
+		y++;
+	}
+
+	return (upper);
+
+}
+
