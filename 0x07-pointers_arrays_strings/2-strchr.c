8ca036ed7db217de1f473eef155865e38d2f8e40
Gogo Progress <progressgogochinda@gmail.com>
2023-10-26 16:39:45 +0100

    pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr -  a function that locates a character in a string.
+ * @s: first parameter of the function
+ * @c: second parameter of the function
+ * Return: returns a string of characters
+ */
+
+char *_strchr(char *s, char c)
+{
+	int y;
+
+	y = 0;
+	while (s[y])
+	{
+		if (s[y] == c)
+		{
+			return (s + y);
+		}
+		y++;
+	}
+	return (NULL);
+}
+
