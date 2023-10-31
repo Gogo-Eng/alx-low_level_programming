44c82f615c56cc5a369edeb0014b452d29c372ff
Gogo Progress <progressgogochinda@gmail.com>
2023-10-31 21:32:36 +0100

    hard things

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *str;
 
+	if (size == 0)
+		return (NULL);
+
 	str = (char *)malloc(size * sizeof(char));
+
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
