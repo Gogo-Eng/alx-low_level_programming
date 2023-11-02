006c69fde434eec978ff3ccf71f358599865a817
Gogo Progress <progressgogochinda@gmail.com>
2023-11-02 01:11:59 +0100

    i stand like moses

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ * @argc: the counter
+ * @argv: the vector
+ * Return: return an integer
+ */
+
+int main(int argc, char *argv[])
+{
+
+		(void)argv;
+		printf("%d\n", argc - 1);
+
+
+	return (0);
+}
+
