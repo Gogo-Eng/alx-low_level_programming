aff538ed5bfb10d9ac650675209c7493416eac5e
Gogo Progress <progressgogochinda@gmail.com>
2023-11-03 12:54:28 +0100

    min et max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range -  a function that creates an array of integers.
+ * @min: first parameter of the function
+ * @max: second parameter of the function
+ * Return: return an integer
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int p;
+	int range = (max - min) + 1;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ptr = (int *)malloc(range * sizeof(int));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (p = min; p <= max; min++, p++)
+	{
+		ptr[p] = min;
+	}
+	return (ptr);
+}
+
