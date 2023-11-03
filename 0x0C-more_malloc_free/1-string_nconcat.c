3cdf59c4d2b8bf8f689d67bd6107a25040c7e9e0
Gogo Progress <progressgogochinda@gmail.com>
2023-11-03 03:55:46 +0100

    your mercy said no

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,7 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[r] = s2[t];
 	}
-	for (v = 0; n >= j && v < (y + j); r++, v++)
+	for (v = 0; n >= j && v < (y + n); r++, v++)
 	{
 		str[r] = s2[v];
 	}
