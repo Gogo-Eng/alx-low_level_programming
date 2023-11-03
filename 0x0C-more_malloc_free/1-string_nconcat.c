4b85026d74c355131ddec3ccf2574e6289ea3a01
Gogo Progress <progressgogochinda@gmail.com>
2023-11-03 03:52:39 +0100

    your mercy said no

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 /**
  * string_nconcat -  a function that concatenates two strings.
@@ -14,43 +13,42 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int y, j, r, t;
+	unsigned int y, j, r, t, v;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-
 	for (y = 0; s1[y]; y++)
 
 	for (j = 0; s2[j] && n > 0; j++)
-
-
-	str = (char *)malloc((y + n + 1) * sizeof(char));
+	if (n < j)
+	{
+		str = (char *)malloc((y + n + 1) * sizeof(char));
+	}
+	else
+	{
+		str = (char *)malloc((y + j + 1) * sizeof(char));
+	}
 
 	if (str == NULL)
 	{
 		free(str);
 	return (NULL);
 	}
-
 	for (r = 0; r < y; r++)
 	{
 		str[r] = s1[r];
 	}
-
-	for (t = 0; t < n; r++, t++)
+	for (t = 0; n < j && t < n; r++, t++)
 	{
 		str[r] = s2[t];
 	}
-
+	for (v = 0; n >= j && v < (y + j); r++, v++)
+	{
+		str[r] = s2[v];
+	}
 	str[r] = '\0';
-
 	return (str);
-
 }
 
