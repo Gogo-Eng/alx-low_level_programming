c9ceacb8ebebbccc7c655664da27b19f219ab9e5
Gogo Progress <progressgogochinda@gmail.com>
2023-11-04 15:34:03 +0100

    gogo

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -44,7 +44,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (!ptr)
 	{
-		newPtr =(char *)malloc(sizeof(char) * new_size);
+		newPtr = malloc(new_size);
 		if (newPtr == NULL)
 		{
 			return (NULL);
@@ -59,6 +59,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	else
 	{
 		newPtr = (char *)malloc(sizeof(char) * new_size);
+		if (newPtr == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}	
 		if (newPtr)
 		{
 			_memcpy(newPtr, ptr, old_size);
