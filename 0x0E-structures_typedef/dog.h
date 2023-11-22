ce2c6d93183aea51adc69d3d72dfa9f5f63574b6
Gogo <progressgogochinda@gmail.com>
2023-11-22 12:22:32 +0100

    structure data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef FRONTMAN
+#define FRONTMAN
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - a struct data type
+ * @name: pointer to a character
+ * @age: a floating point integer
+ * @owner: pointer to a character
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
