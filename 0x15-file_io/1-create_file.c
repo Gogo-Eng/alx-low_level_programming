c041ae1bc23a7bb6c5bd9c3ffb4db6d78b477568
Gogo <progressgogochinda@gmail.com>
2023-12-01 18:55:44 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		writing = write(point, text_content, strlen(text_content));
-		
+
 		if (writing == -1)
 		{
 			printf("unable to write to file");
