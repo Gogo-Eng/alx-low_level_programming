1ec7e8923eef223360def3a2a4e212f7f6649955
Gogo <progressgogochinda@gmail.com>
2023-12-01 19:52:48 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int point, writing;
+	int point, writing, length;
 
 	if (filename == NULL)
 	{
@@ -18,16 +18,16 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	point = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (point == -1)
-	{
-		printf("file can not open");
-		return (-1);
-	}
-
 	if (text_content != NULL)
 	{
+		for (length = 0; text_content[length];)
+		{
+			length++;
+		}
+	}
+
+
+		point = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		writing = write(point, text_content, strlen(text_content));
 
 		if (writing == -1)
@@ -35,8 +35,12 @@ int create_file(const char *filename, char *text_content)
 			printf("unable to write to file");
 			return (-1);
 		}
+		if (point == -1)
+		{
+			printf("file can not open");
+			return (-1);
+		}
 
 		close(point);
-	}
-	return (1);
+		return (1);
 }
