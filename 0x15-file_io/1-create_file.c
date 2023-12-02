72acce687d6ba8b9fa3f316b48f2f0799e63d6ce
Gogo <progressgogochinda@gmail.com>
2023-12-02 05:46:32 +0100

    commited

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,35 +7,21 @@
  * @text_content: number od letters
  * Return: Returns: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int point, writing, length;
+	int fd, wt;
 
 	if (filename == NULL)
-	{
-		printf("file is empty");
 		return (-1);
-	}
-
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
-		{
-			length++;
-		}
-	}
-
-
-		point = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		writing = write(point, text_content, length);
-
-		if (point == -1 || writing == -1)
-		{
-
+		wt = write(fd, text_content, strlen(text_content));
+		if (wt == -1)
 			return (-1);
-		}
-
-		close(point);
-		return (1);
+	}
+	close(fd);
+	return (1);
 }
