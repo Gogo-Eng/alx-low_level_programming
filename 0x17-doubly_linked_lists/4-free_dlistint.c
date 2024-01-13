43daf726d497e863f4055f949d96c6e73f158790
Gogo <progressgogochinda@gmail.com>
2024-01-13 16:20:16 +0100

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - free_dlistintfree_dlistint
+ * @head: pointer to the first node
+ * Return: 0
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free (temp);
+	}
+}
